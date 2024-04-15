f41b4c51ffd00a863521593d6165428af1cf962b
KiriTamazo <kiritokirikaya80@gmail.com>
2024-04-15 20:26:51 +0630

    FF - adding product list

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,18 @@
 import products from "@/assets/data/products";
 import ProductListItem from "@/src/components/pages/ProductListItem";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
 export default function TabOneScreen() {
   return (
-    <View>
-      {products?.map((product) => (
-        <ProductListItem product={product} key={product?.id} />
-      ))}
+    <View >
+      <FlatList
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+        numColumns={2} data={products}
+        renderItem={({ item }) => (
+          <ProductListItem product={item} key={item?.id} />
+        )}
+      />
     </View>
   );
 }
--- src/components/pages/ProductListItem.tsx
+++ src/components/pages/ProductListItem.tsx
@@ -1,14 +1,19 @@
+import { defaultPizzaImage } from "@/assets/data/products";
 import { ProductsProps } from "@/src/types";
 import { Image, StyleSheet, Text, View } from "react-native";
 type ProductListItemProp = {
-product:ProductsProps;
+  product: ProductsProps;
 }
 const ProductListItem = ({ product }: ProductListItemProp) => {
   return (
-    <View className="bg-white p-5 rounded">
-      <Image source={{ uri: product?.image }} className="w-full aspect-[1]" />
-      <Text className="font-semibold text-lg">{product?.name}</Text>
-      <Text>{product?.price}</Text>
+    <View className="bg-white flex-1 max-w-[50%] p-2 rounded">
+      <Image resizeMode="contain"
+        source={{ uri: product?.image || defaultPizzaImage }} className="w-full aspect-[1] mb-2"
+      />
+      <Text className="font-semibold text-lg">
+        {product?.name}
+      </Text>
+      <Text>${product?.price}</Text>
     </View>
   );
 };
