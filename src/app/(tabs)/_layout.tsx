f85966a61f738195676c0b76d292557b198ad4b7
LW-Htet-Aung <kiritokirikaya80@gmail.com>
2024-05-05 04:05:33 +0630

    FF - added user and admin layout in component

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { MaterialCommunityIcons } from '@expo/vector-icons'; import { Link, Tabs } from "expo-router";
-import { Pressable } from "react-native";
 
 import Colors from "@/src/constants/Colors";
 import { useColorScheme } from "@/components/useColorScheme";
 import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+import { useIsAdmin } from "@/src/stores/authStore";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -16,14 +16,19 @@ function TabBarIcon(props: {
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const admin = useIsAdmin()
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarActiveTintColor: admin ? Colors.light.background : Colors.light.tint,
+        tabBarInactiveTintColor: admin ? Colors.light.text : Colors.light.text,
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
+        tabBarStyle: {
+          backgroundColor: admin ? Colors.light.tint : Colors.light.background,
+        }
       }}
     >
       <Tabs.Screen name='index' options={{ href: null }} />
