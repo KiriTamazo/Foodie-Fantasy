3c47e44accada53d90083842d6aa4fb1dacec23f
LW-Htet-Aung <kiritokirikaya80@gmail.com>
2024-05-05 04:05:52 +0630

    FF - Admin Product Create

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,5 +1,6 @@
 import products from "@/assets/data/products";
 import Button from "@/src/components/ui/Button";
+import { useIsAdmin } from "@/src/stores/authStore";
 import { useCartStore } from "@/src/stores/cartStore";
 import { PizzaSize } from "@/src/types";
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
@@ -9,11 +10,14 @@ const sizes = ['S', 'M', 'L', 'XL'];
 const ProductDetail = () => {
     const { id } = useLocalSearchParams();
     const router = useRouter()
-    const product = products.find((product) => product.id.toString() === id);
-    const [selectedSize, setSelectedSize] = useState<PizzaSize>("S");
+    const admin = useIsAdmin()
     const addToCart = useCartStore((state) => state.addToCart)
 
+    const product = products.find((product) => product.id.toString() === id);
+    const [selectedSize, setSelectedSize] = useState<PizzaSize>("S");
+
     if (!product) return <Text>Product Not Found</Text>;
+
     const handleAddToCart = () => {
         addToCart(product, selectedSize)
         router.push('/cart')
@@ -26,26 +30,33 @@ const ProductDetail = () => {
                 }}
             />
             <Image className="w-full aspect-[1]" source={{ uri: product.image }} />
-            <Text className="text-lg font-semibold">Select Size</Text>
-            <View className="flex-row justify-around gap-4 my-[10px]">
-                {sizes.map((size) => (
-                    <Pressable
-                        key={size}
-                        onPress={() => setSelectedSize(size as PizzaSize)}
-                        className={`${selectedSize === size ? 'bg-gray-200' : ''} w-[50px] aspect-[1] items-center justify-center rounded-full`}
-                    >
-                        <Text
-                            className={`${selectedSize !== size ? 'text-gray-600' : ''} text-[20px] capitalize font-medium`}
-                            key={size}>
-                            {size}
-                        </Text>
-                    </Pressable>
-                ))}
-            </View>
-            <Text className="text-lg font-bold mt-auto">
-                ${product.price}
-            </Text>
-            <Button onPress={handleAddToCart} text='add to cart' />
+            {admin ?
+                (<Text>
+                    View
+                </Text>) :
+                (<>
+
+                    <Text className="text-lg font-semibold">Select Size</Text>
+                    <View className="flex-row justify-around gap-4 my-[10px]">
+                        {sizes.map((size) => (
+                            <Pressable
+                                key={size}
+                                onPress={() => setSelectedSize(size as PizzaSize)}
+                                className={`${selectedSize === size ? 'bg-gray-200' : ''} w-[50px] aspect-[1] items-center justify-center rounded-full`}
+                            >
+                                <Text
+                                    className={`${selectedSize !== size ? 'text-gray-600' : ''} text-[20px] capitalize font-medium`}
+                                    key={size}>
+                                    {size}
+                                </Text>
+                            </Pressable>
+                        ))}
+                    </View>
+                    <Text className="text-lg font-bold mt-auto">
+                        ${product.price}
+                    </Text>
+                    <Button onPress={handleAddToCart} text='add to cart' /></>
+                )}
         </View>
     );
 };
---
+++ src/app/(tabs)/menu/create.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+type Props = {}
+const CreateRecipe = (props: Props) => {
+    return (
+        <View>
+            <Text> CreateRecipe</Text>
+        </View >
+    )
+}
+export default CreateRecipe
+const styles = StyleSheet.create({})
