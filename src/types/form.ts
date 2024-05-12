c6fb571c83bd667d64743ab96e64931fd0c3ab43
LW-Htet-Aung <kiritokirikaya80@gmail.com>
2024-05-12 17:08:37 +0630

    FF - added form action

--- src/types/form.ts
+++ src/types/form.ts
@@ -9,6 +9,14 @@ export type FormFieldProps = {
   error: FieldError | undefined;
   valueAsNumber?: boolean;
 };
+export type SignInType = {
+  email: string;
+  password: string;
+};
+export type SignUpType = {
+  name: string;
+  confirm_password: string;
+};
 
 export type ValidFieldNames = "name" | "price";
 
@@ -17,5 +25,20 @@ export const ProductSchema = z.object({
   price: z.string(),
   image: z.string().nullable(),
 });
+
+export const SignInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(4),
+});
+export const SignUpSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(4),
+    confirm_password: z.string().min(4),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: "Password does not match",
+    path: ["confirm_password"],
+  });
 
 export type ProductSchemaType = z.infer<typeof ProductSchema>;
